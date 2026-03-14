2c06295dc7ac3e10525239082f1bb365400aa727
Thomas Newman <tnewman@users.noreply.github.com>
2026-03-13 20:09:02 -0400

    Added CI workflow

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     `java-library`
     alias(libs.plugins.spotless)
     alias(libs.plugins.jmh)
+    alias(libs.plugins.maven.publish)
 }
 
 repositories {
@@ -16,17 +17,21 @@ dependencies {
     jmhAnnotationProcessor(libs.jmh.annprocess)
 }
 
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(17)
+    }
+}
+
+tasks.named<Test>("test") {
+    useJUnitPlatform()
+}
+
 jmh {
     fork.set(1)
     warmupIterations.set(3)
     iterations.set(5)
     benchmarkMode.set(listOf("thrpt"))
-}
-
-java {
-    toolchain {
-        languageVersion = JavaLanguageVersion.of(17)
-    }
 }
 
 spotless {
@@ -35,6 +40,39 @@ spotless {
     }
 }
 
-tasks.named<Test>("test") {
-    useJUnitPlatform()
+val group: String by project
+val artifact: String by project
+val version: String by project
+
+mavenPublishing {
+  publishToMavenCentral()
+  signAllPublications()
+
+  coordinates(group, artifact, version)
+
+  pom {
+    name.set("Sliding Window Counter")
+    description.set("A Java library implementing a concurrent sliding window counter.")
+    inceptionYear.set("2026")
+    url.set("https://github.com/tnewman/sliding-window-counter-java")
+    licenses {
+      license {
+        name.set("The Apache License, Version 2.0")
+        url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+        distribution.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+      }
+    }
+    developers {
+      developer {
+        id.set("tnewman")
+        name.set("Thomas Newman")
+        url.set("https://github.com/tnewman")
+      }
+    }
+    scm {
+      url.set("https://github.com/tnewman/sliding-window-counter-java")
+      connection.set("scm:git:https://github.com/tnewman/sliding-window-counter-java.git")
+      developerConnection.set("scm:git:https://github.com/tnewman/sliding-window-counter-java.git")
+    }
+  }
 }
